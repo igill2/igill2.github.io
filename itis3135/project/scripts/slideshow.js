0cbadfc5020d1cb4298ec80c99f080f72bd2bc67
iyangill <iyangill64@gmail.com>
2021-12-13 13:57:33 -0500

    file changes

--- itis3135/project/scripts/slideshow.js
+++ itis3135/project/scripts/slideshow.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
       $('.slider').bxSlider();
     });
+$(document).ready(function(){
+  $("button").click(function(){
+    $.ajax({url: "demo_test.txt", success: function(result){
+      $("#div1").html(result);
+    }});
+  });
+});
