f6befa182776dd4b5d41c85a79bbf8be53b6711b
iyangill <iyangill64@gmail.com>
2021-12-13 11:43:41 -0500

    finally

--- itis3135/project/scripts/slideshow.js
+++ itis3135/project/scripts/slideshow.js
@@ -1,19 +1,3 @@
 $(document).ready(function(){
-    //preload images
-    $("#image_list").each( (index, link) => {
-        const image = new Image();
-        image.src = link.href;
+      $('.slider').bxSlider();
     });
-
-    //attach event handlers for links
-    $("#image_list").click( evt => {
-        //get the clicked <a> tag
-        const link = evt.currentTarget;
-    })
-
-    //Call bxSlider in order to create the slideshow using the images
-    $('.bxslider').bxSlider();
-
-
-});
-
